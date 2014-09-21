8b2fb39a78ebec4f45a71df1d57561e60de855d3
Ian Paul <ian@ianpaul.net>
2014-09-21 13:07:24 +0300

    got-table-working
    Table is working in rudimentary form. Now on to packaging all this in a
    function.

--- seaweed.js
+++ seaweed.js
@@ -1,35 +1,17 @@
-var beaches = ["Hilton Beach", "Hazuk Beach", "Dolphinarium", "Topsy Beach", "Herzliya Marina", 
+// Get array of classes without jQuery
+var array = ["Hilton Beach", "Hazuk Beach", "Dolphinarium", "Topsy Beach", "Herzliya Marina", 
 "Hof Maravi", "Beit Yanai"];
 
-var buildBeaches = function () {
-	document.getElementById('beachName1').innerHTML = beaches[0];
-	document.getElementById('beachName2').innerHTML = beaches[1];
-	document.getElementById('beachName3').innerHTML = beaches[2];
-	document.getElementById('beachName4').innerHTML = beaches[3];
-	document.getElementById('beachName5').innerHTML = beaches[4];
-	document.getElementById('beachName6').innerHTML = beaches[5];
-	document.getElementById('beachName7').innerHTML = beaches[6];
+var arrayLength = array.length;
+var theTable = document.createElement('table');
 
+// Note, don't forget the var keyword!
+for (var i = 0, tr, td; i < arrayLength; i++) {
+    tr = document.createElement('tr');
+    td = document.createElement('td');
+    td.appendChild(document.createTextNode(array[i]));
+    tr.appendChild(td);
+    theTable.appendChild(tr);
 }
 
-document.addEventListener('load', function(){
-var makeTable = document.getElementById('beachTable');
-makeTable.addEventListener('load', buildBeaches);
-});
-
-
-var makeWindow1 = function () {
-	chrome.app.window.create('window1.html', {
-    'bounds': {
-      'width': 400,
-      'height': 500
-    }
-  });
-}
-
-
-document.addEventListener('DOMContentLoaded', function() {
-	var movePage = document.getElementById('nextPage');
-	//move to next html page
-	movePage.addEventListener('click', makeWindow1);
-});
+document.getElementById('table').appendChild(theTable);
