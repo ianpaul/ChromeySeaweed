1a875fb024fac8487c79d80a5dd8cb625489b2f2
Ian Paul <ian@ianpaul.net>
2014-09-21 17:13:29 +0300

    table is fed.
    Time to move on to the next thing.

--- seaweed.js
+++ seaweed.js
@@ -1,9 +1,11 @@
 // Get array of classes without jQuery
-var array = ["Hilton Beach", "Hazuk Beach", "Dolphinarium", "Topsy Beach", "Herzliya Marina", 
+var beaches1 = ["Hilton Beach", "Hazuk Beach", "Dolphinarium", "Topsy Beach", "Herzliya Marina", 
 "Hof Maravi", "Beit Yanai"];
 
-var arrayLength = array.length;
+var arrayLength = beaches1.length;
 var theTable = document.createElement('table');
+
+var buildBeaches = function (array) {
 
 // Note, don't forget the var keyword!
 for (var i = 0, tr, td; i < arrayLength; i++) {
@@ -15,3 +17,7 @@ for (var i = 0, tr, td; i < arrayLength; i++) {
 }
 
 document.getElementById('table').appendChild(theTable);
+
+}
+
+buildBeaches(beaches1);
